d9b805631dbad3b40b02f94f8bd3e204f7dcb2ee
JYOTI-MARKUN <jyotimarkun1997@gmail.com>
2023-09-27 21:25:02 +0530

    create a new repo

---
+++ hellow.py
@@ -0,0 +1,7 @@
+a=int(input("enter any number:"))
+b=int(input("enter any number:"))
+c=int(input("enter any number:"))
+d=int(input("enter any number:"))
+e=a+b+c+d
+print("the sum of all the numbers is:",e)
+
